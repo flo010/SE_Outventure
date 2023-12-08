18ed507f53f0acfd4f52f1267edffd79211d3b0d
Betül <kulac.esrabetul@hotmail.com>
2023-12-08 12:09:49 +0100

    fixing bug delete and edit buton in details

--- src/main/webapp/hike_detail/hike_detail.js
+++ src/main/webapp/hike_detail/hike_detail.js
@@ -1,9 +1,3 @@
-// delete hike functions
-function deleteHike(hikeID) {
-    // Redirect to the DeleteHikeServlet
-    window.location.href = '../delete_hike?hikeID=' + hikeID;
-}
-
 function showDeleteHikeModal() {
     let deleteHikeModal = new bootstrap.Modal(document.getElementById('deleteHikeModal'), {
         keyboard: false
