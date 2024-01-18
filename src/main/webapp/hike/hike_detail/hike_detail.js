bb71d81dd6cf9576a2a276509bb1d5cabd5619f1
Ilayde Kocak <iko1260@students.fhv.at>
2024-01-18 21:31:28 +0100

    gpx export download fixed

--- src/main/webapp/hike/hike_detail/hike_detail.js
+++ src/main/webapp/hike/hike_detail/hike_detail.js
@@ -200,6 +200,7 @@ function createGPX() {
         '</gpx>';
 }
 
+
 function updateFavorites(hikeID, hikerID) {
     window.location.href = '/favorite_hike?hikeID=' + hikeID + '&hikerID=' + hikerID + '&page=detail';
 }
